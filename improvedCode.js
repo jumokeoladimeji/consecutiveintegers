d55f5d2a077ea90396a7637b71de42e1a1475bd3
jumokeoladimeji <jumoke5ng@yahoo.com>
2021-05-06 14:16:09 +0100

    fixed code

--- improvedCode.js
+++ improvedCode.js
@@ -1,38 +1,17 @@
 const getTrendArray = (integerArray) => {
-    let countUp = 0;
-    let countDown = 0;
-    let previousInteger = integerArray[0];
-    let firstTrendIndex = -1;
-    let uptrend = false;
-    let downtrend = false;
-
-    for (let i=1; i < integerArray.length; i++) {
-        if(previousInteger < integerArray[i]) { 
-            countUp++;
-            countDown = 0;
-            uptrend =  true;
-            downtrend = false;
-            firstTrendIndex = (countUp === 1) ? i : firstTrendIndex;
+    for (let i=0; i < integerArray.length; i++) {
+        if(integerArray[i] < integerArray[i+1] && integerArray[i+1] < integerArray[i+2]) { 
+            console.log(integerArray[i], integerArray[i+1], integerArray[i+2])
+          return [true, false, i];
         }
-        if(previousInteger > integerArray[i]) {
-            countDown++;
-            countUp = 0;
-            downtrend = true;
-            uptrend = false;
-            firstTrendIndex = (countDown === 1) ? i : firstTrendIndex;
+         if(integerArray[i] > integerArray[i+1] && integerArray[i+1] > integerArray[i+2]) { 
+             console.log(integerArray[i], integerArray[i+1], integerArray[i+2])
+          return [false, true, i];
         }
-        if (countDown === 3 || countUp === 3) {
-          return [uptrend, downtrend, firstTrendIndex];
-        }
-        previousInteger = integerArray[i];
-
     }
-    if(countUp !== 3 && countDown !== 3){
-          firstTrendIndex = -1
-    }
-    return [uptrend, downtrend, firstTrendIndex];
+    return [false, false, -1];
 }
 
 console.log('downtrend', getTrendArray([57, 5, 30, 19, 18, 12, 7, 18, 22, 32, 20, 11, 8, 5]));
 console.log('uptrend', getTrendArray([32, 5, 11, 7, 8, 10, 11, 18, 5, 2]));
-console.log('false', getTrendArray([32, 12, 15, 11, 22, 11, 18, 18, 15, 7]));
+console.log('false', getTrendArray([32, 12, 15, 11, 22, 11, 18, 18, 15, 12]));
