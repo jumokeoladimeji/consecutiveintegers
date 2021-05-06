9f2303012888d1a25fbc36efec044d8f7260e105
jumokeoladimeji <jumoke5ng@yahoo.com>
2021-05-06 14:19:50 +0100

    change test case for when there is no trend

--- improvedCode.js
+++ improvedCode.js
@@ -14,4 +14,4 @@ const getTrendArray = (integerArray) => {
 
 console.log('downtrend', getTrendArray([57, 5, 30, 19, 18, 12, 7, 18, 22, 32, 20, 11, 8, 5]));
 console.log('uptrend', getTrendArray([32, 5, 11, 7, 8, 10, 11, 18, 5, 2]));
-console.log('false', getTrendArray([32, 12, 15, 11, 22, 11, 18, 18, 15, 12]));
+console.log('false', getTrendArray([32, 12, 15, 11, 22, 11, 18, 18, 15, 17]));
